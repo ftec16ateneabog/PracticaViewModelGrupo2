acce94668c1e35539fec1372e352b73b11c7c184
ftec16ateneabog <ftec16atenea_bog@unal.edu.co>
2023-11-23 18:40:37 -0500

    android example

--- app/src/main/java/com/atenea/practicaviewmodel/MainActivity.java
+++ app/src/main/java/com/atenea/practicaviewmodel/MainActivity.java
@@ -1,13 +1,26 @@
 package com.atenea.practicaviewmodel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.atenea.practicaviewmodel.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private MyViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        binding.setViewModel(viewModel);
+
+        binding.setLifecycleOwner(this);
     }
 }
